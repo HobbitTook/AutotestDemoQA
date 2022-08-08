07751713aab826e67c1f8435ac6c7dd8aad2186c
HobbitTook <hobbittook@gmail.com>
2022-08-08 17:58:47 +0300

    Autotest GitHub - Drag&Drop

---
+++ src/test/java/github2/DragDrop.java
@@ -0,0 +1,36 @@
+package github2;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DragDrop {
+
+    @BeforeAll
+    static void configure() {
+
+        Configuration.browser = "chrome";
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1980";
+    }
+
+    @AfterAll
+    static void configure1() {
+
+        Configuration.holdBrowserOpen = false;
+    }
+
+    @Test
+    void testDragDrop(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+}
+
