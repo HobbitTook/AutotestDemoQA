22096da24697c4e28d5d395fe8bc8ce77c98aeea
HobbitTook <hobbittook@gmail.com>
2022-08-25 20:02:40 +0300

    Autotest Step and Allure

---
+++ src/test/java/github/AllureTests/GitQaGuruTest.java
@@ -0,0 +1,29 @@
+package github.AllureTests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class GitQaGuruTest {
+
+        @Test
+        public void testIssueSearch () {
+            SelenideLogger.addListener("allure", new AllureSelenide());
+
+            open("https://github.com");
+
+            $(".header-search-input").click();
+            $(".header-search-input").sendKeys("qa_guru_14_10");
+            $(".header-search-input").submit();
+
+            $("a[href*='qa-guru/qa_guru_14_10']").click();
+            $("#issues-tab").shouldHave();
+
+        }
+    }
---
+++ src/test/java/github/AllureTests/StepsQaGuruTest.java
@@ -0,0 +1,51 @@
+package github.AllureTests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+
+public class StepsQaGuruTest {
+    private static final String REPOSITORY = "qa_guru_14_10";
+
+    @Test
+    public void testLambdaStep() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открыть главную страницу", () -> {
+            open("https://github.com");
+        });
+        step("Найти репозиторий " + REPOSITORY, () -> {
+            $(".header-search-input").click();
+            $(".header-search-input").sendKeys(REPOSITORY);
+            $(".header-search-input").submit();
+        });
+        step("Кликнуть по ссылке репозитория" + REPOSITORY, () -> {
+            $("a[href*='qa-guru/qa_guru_14_10']").click();
+        });
+        step("Найти раздел Issues", () -> {
+            $(withText("Issue")).should(Condition.exist);
+        });
+    }
+
+    @Test
+    public void testAnnotatedStepAttachment() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebSteps steps = new WebSteps();
+        steps.openMainPage();
+        steps.searchForRepository(REPOSITORY);
+        steps.clickOnRepositoryLink(REPOSITORY);
+        steps.seachIssuesTab();
+        steps.takeScreenshot();
+
+    }
+}
