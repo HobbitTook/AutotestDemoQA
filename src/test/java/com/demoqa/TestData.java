b0c2e60b67655fcb20629800fb7512fd00337aa1
HobbitTook <hobbittook@gmail.com>
2022-09-08 16:49:44 +0300

    Autotest Jenkins + TestData version browser

--- src/test/java/com/demoqa/TestData.java
+++ src/test/java/com/demoqa/TestData.java
@@ -21,7 +21,7 @@ public class TestData {
         Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browser = System.getProperty("browser_name", "chrome");
-        Configuration.browserVersion = System.getProperty("browser_version", "104.0");
+        Configuration.browserVersion = System.getProperty("browser_version", "105.0");
         Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
 
         if (System.getProperty("selenide.remote") != null) {
